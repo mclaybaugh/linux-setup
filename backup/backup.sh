a7601412365bc17fa05d949fdd3108c17f649216
Michael Claybaugh <michael.claybaugh@gmail.com>
2018-06-25 08:06:06 -0400

    added human-readable, removed unused lines

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
-# Syncing files in ~/Backup/ to digital ocean droplet
-#	Options used:
-#		r -> recursive
-#		v -> verbose ~/Backup/ -> copy CONTENTS of Backup 
-#		--exclude 'node_modules' -> excludes all node_module directories to avoid backing that crap up
+# Syncing files in ~/Backup/ to personal server
 
 check_file_status () {
-#	echo "Checking local files..."
-#	rsync --list-only --recursive --verbose \
-#		--exclude-from='./exclude.txt' ~/Backup/ \
-#		> temp1 
-#
-#	echo "Checking remote files..."
-#	rsync --list-only --recursive --verbose \
-#		--exclude-from='./exclude.txt' michael@michaelclaybaugh.com:~/Backup/ \
-#		> temp2
-#
-#	echo "Checking difference..."
-#	diff temp1 temp2
-
-	rsync --recursive --verbose --update --dry-run \
+	rsync --recursive --verbose --update --dry-run --human-readable \
 		--exclude-from='exclude.txt' \
 		~/Backup/ \
 		michael@michaelclaybaugh.com:~/Backup/
 }
 
 backup_files () {
-	rsync --recursive --verbose \
+	rsync --recursive --verbose --human-readable \
 		--exclude-from='exclude.txt' \
 		~/Backup/ \
 		michael@michaelclaybaugh.com:~/Backup/
 }
 
 #check_file_status
-backup_files
+#backup_files
