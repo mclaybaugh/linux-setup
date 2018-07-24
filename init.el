1e732a74df3604ff02d82dc65a91424d7083e183
Michael Claybaugh <michael.claybaugh@gmail.com>
2018-07-24 11:57:09 -0400

    Added melpa, disabled toolbar, added gruvbox theme

--- init.el
+++ init.el
@@ -1,1 +1,35 @@
+;; Michael Claybaugh's emacs init file
+
+;; MELPA see https://github.com/melpa/melpa
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (markdown-mode gruvbox-theme rust-mode web-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Disable audible bell because reasons
 (setq visible-bell 1)
+					;
+;; Disable toolbar
+(tool-bar-mode -1)
+
+;; Colortheme
+(load-theme 'gruvbox t)
