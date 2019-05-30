3329357c18434385f0ae120c618b799300851e19
Michael Claybaugh <mclaybaugh2@liberty.edu>
2019-05-30 14:06:44 -0400

    migrate to fish shell

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
-echo -n Setting abbreviations...
+# variables
+set FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
+# Abbreviations
 abbr gd "find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;"
-
-echo "Done"
---
+++ fish-onetime-setup.fish
@@ -0,0 +1,2 @@
+# add ~/bin to path persistently
+set -U fish_user_paths $fish_user_paths ~/bin
