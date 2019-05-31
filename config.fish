8ab72c2e2442865020cad647854ca0daadfccc93
Michael Claybaugh <mclaybaugh2@liberty.edu>
2019-05-31 14:54:26 -0400

    instructions for fish and nvm

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 # variables
-set FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
 # Abbreviations
 abbr gd "find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;"
