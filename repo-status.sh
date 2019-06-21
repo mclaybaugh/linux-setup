85e45ce905c9cdc7345ffb3e1c4030452b98a1fc
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-06-21 14:31:12 -0400

    update repo-status directory names

--- repo-status.sh
+++ repo-status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 find ~/docker/edu/site/wp-content/themes/ -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;
-find ~/remote-repositories/ -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;
-find ~/remote-repositories/liberty/ -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;
+find ~/repositories/ -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;
+find ~/repositories/liberty/ -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s && echo)' \;
