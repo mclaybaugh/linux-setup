dfc81e0766c90e2f4dd8b3401052746cb6d12c8c
Michael Claybaugh <mclaybaugh2@liberty.edu>
2019-08-12 12:22:29 -0400

    refactored after install pylint for python3

--- journal.py
+++ journal.py
@@ -1,28 +1,31 @@
 #!/usr/local/bin/python3
 # journal.py -- will auto generate log files
-#
-# Check if today's file already exists:
-#    exists -> message and exit
-#    otherwise -> prompt for title
 
 import os
 import datetime
 
-# returns array of strings
+def main():
+    """
+    If journal file already exists, print filenames and exit. Otherwise,
+    prompt for title and create journal file
+    """
+    today = datetime.datetime.now()
+    date = today.strftime('%Y-%m-%d')
+    matching = [name for name in working_directory_files() if date in name]
+
+    if matching:
+        print('Existing files found:')
+        for name in matching:
+            print(name)
+    else:
+        title = input('Enter Title [journal]: ')
+        if title == '':
+            title = 'journal'
+        with open(f'{date}_{title}.md', 'w') as file:
+            file.write(f'# {title}')
+
 def working_directory_files():
+    """Get list of file names in current working directory"""
     return [file.name for file in os.scandir() if file.is_file()]
 
-today = datetime.datetime.now()
-date = today.strftime('%Y-%m-%d')
-matching = [name for name in working_directory_files() if date in name]
-
-if len(matching) > 0:
-    print('Existing files found:')
-    for f in matching:
-        print(f)
-else:
-    title = input("Enter Title [journal]: ")
-    if title == '':
-        title = 'journal'
-    with open(f'{date}_{title}.md', 'w') as f:
-        f.write(f'# {title}')
+main()
