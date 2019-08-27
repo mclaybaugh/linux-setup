acee317cc819cee6ce465debf55edd35a38ed02f
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-08-27 15:19:07 -0400

    moved main into root level

--- journal.py
+++ journal.py
@@ -4,28 +4,26 @@
 import os
 import datetime
 
-def main():
-    """
-    If journal file already exists, print filenames and exit. Otherwise,
-    prompt for title and create journal file
-    """
-    today = datetime.datetime.now()
-    date = today.strftime('%Y-%m-%d')
-    matching = [name for name in working_directory_files() if date in name]
-
-    if matching:
-        print('Existing files found:')
-        for name in matching:
-            print(name)
-    else:
-        title = input('Enter Title [journal]: ')
-        if title == '':
-            title = 'journal'
-        with open(f'{date}_{title}.md', 'w') as file:
-            file.write(f'# {title}')
-
 def working_directory_files():
     """Get list of file names in current working directory"""
     return [file.name for file in os.scandir() if file.is_file()]
 
-main()
+
+"""
+If journal file already exists, print filenames and exit. Otherwise,
+prompt for title and create journal file
+"""
+today = datetime.datetime.now()
+date = today.strftime('%Y-%m-%d')
+matching = [name for name in working_directory_files() if date in name]
+
+if matching:
+    print('Existing files found:')
+    for name in matching:
+        print(name)
+else:
+    title = input('Enter Title [journal]: ')
+    if title == '':
+        title = 'journal'
+    with open(f'{date}_{title}.md', 'w') as file:
+        file.write(f'# {title}')
