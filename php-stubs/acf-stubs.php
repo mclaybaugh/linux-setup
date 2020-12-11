56bdc05b6567430492b85e0d9df00a864f924218
Michael Claybaugh <michael.claybaugh@gmail.com>
2020-12-11 12:54:42 -0500

    php intelephense stubs for advanced custom fields

---
+++ php-stubs/acf-stubs.php
@@ -0,0 +1,9 @@
+<?php
+
+function get_field(string $selector, $post_id = null, bool $format_value = true): array|string|object|int {}
+
+function acf_add_options_page(array $settings) {}
+
+function get_fields($post_id = null, bool $format_value = null): array|false {}
+
+function acf_add_local_field_group(array $field_group) {}
