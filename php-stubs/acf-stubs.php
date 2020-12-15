d5c6e99efe3236419ae869e2c86f6832afdbe2b5
Michael Claybaugh <michael.claybaugh@gmail.com>
2020-12-15 09:38:07 -0500

    get_sub_field, have_rows, the_row

--- php-stubs/acf-stubs.php
+++ php-stubs/acf-stubs.php
@@ -1,9 +1,15 @@
 <?php
 
-function get_field(string $selector, $post_id = null, bool $format_value = true): array|string|object|int {}
+function get_field(string $selector, $post_id = null, bool $format_value = true): string|array|object {}
+
+function get_sub_field(string $selector, bool $format_value = true): string|array|object {}
 
 function acf_add_options_page(array $settings) {}
 
 function get_fields($post_id = null, bool $format_value = null): array|false {}
 
 function acf_add_local_field_group(array $field_group) {}
+
+function have_rows(string $selector, int $post_id = false): bool {}
+
+function the_row() {}
