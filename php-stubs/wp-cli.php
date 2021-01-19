a9c105fd916768eeda5805d4fff5d42935c672be
Michael Claybaugh <michael.claybaugh@gmail.com>
2021-01-19 09:18:51 -0500

    apache config examples for local dev

--- php-stubs/wp-cli.php
+++ php-stubs/wp-cli.php
@@ -3,5 +3,6 @@
 class WP_CLI {
     public static function log(string $message) {}
     public static function error(string $message) {}
+    public static function success(string $message) {}
     public static function add_command($name, $callable, $args = []) {}
 }
